07a472715c2cff58dffb33316223245695c580ef
MSenden <mario.senden@googlemail.com>
2019-07-09 10:06:45 +0200

    revert style + revert to experimental procedure in LTI study

--- RM.m
+++ RM.m
@@ -112,122 +112,122 @@ classdef RM < handle
             
             p.parse(varargin{:});
             
-            self.dV         = @(V_rec,V_ff,W,alpha,tau)...
+            self.dV = @(V_rec,V_ff,W,alpha,tau)...
                 (-V_rec+V_ff+W*alpha*max(V_rec,0))/tau;
-            self.Adiff      = @(A,B)...
+            self.Adiff = @(A,B)...
                 angle(exp((A-B)*pi/90*1i))*90/pi;
-            self.Cprob      = @(In,a,c)...
+            self.Cprob = @(In,a,c)...
                 c*(cosd(2*In)+1).^a;
             
-            self.N          = p.Results.N;
-            self.alpha      = p.Results.alpha;
-            self.sigma_ff   = p.Results.sigma_ff;
-            self.J_ff       = p.Results.J_ff;
-            self.J_rec      = p.Results.J_rec;
-            self.a_e        = p.Results.a_e;
-            self.a_i        = p.Results.a_i;
-            self.c_e        = p.Results.c_e;
-            self.c_i        = p.Results.c_i;
-            self.k          = p.Results.k;
-            self.C          = p.Results.C;
-            self.eta        = p.Results.eta;
-            self.Eta        = self.eta*ones(self.N);
-            self.mu         = p.Results.mu;
-            self.tau        = p.Results.tau;
-            self.t_sim      = p.Results.t_sim;
-            self.trials     = p.Results.trials;
-            self.Theta      = linspace(-90,90,self.N)';
-            self.mean_JND   =  0;
-            self.V_0        = zeros(self.N,1);
-            self.W_exc      = self.J_rec * self.Cprob(meshgrid(self.Theta)...
+            self.N = p.Results.N;
+            self.alpha = p.Results.alpha;
+            self.sigma_ff = p.Results.sigma_ff;
+            self.J_ff = p.Results.J_ff;
+            self.J_rec = p.Results.J_rec;
+            self.a_e = p.Results.a_e;
+            self.a_i = p.Results.a_i;
+            self.c_e = p.Results.c_e;
+            self.c_i = p.Results.c_i;
+            self.k = p.Results.k;
+            self.C = p.Results.C;
+            self.eta = p.Results.eta;
+            self.Eta = self.eta*ones(self.N);
+            self.mu = p.Results.mu;
+            self.tau = p.Results.tau;
+            self.t_sim = p.Results.t_sim;
+            self.trials = p.Results.trials;
+            self.Theta = linspace(-90,90,self.N)';
+            self.mean_JND =  0;
+            self.V_0 = zeros(self.N,1);
+            self.W_exc = self.J_rec * self.Cprob(meshgrid(self.Theta)...
                 -meshgrid(self.Theta)',...
                 self.a_e,self.c_e);
-            self.W_inh      = self.J_rec * self.Cprob(meshgrid(self.Theta)...
+            self.W_inh = self.J_rec * self.Cprob(meshgrid(self.Theta)...
                 -meshgrid(self.Theta)',...
                 self.a_i,self.c_i);
-            self.Phi_0      = 135;
-            self.Phi        = self.Phi_0;
-            self.OD_0       = p.Results.OD;
-            self.OD         = self.OD_0;
-            self.p_correct  =  0;
-            self.p_incorrect=  0;
-            self.counter    =  0;
+            self.Phi_0 = 135;
+            self.Phi = self.Phi_0;
+            self.OD_0 = p.Results.OD;
+            self.OD = self.OD_0;
+            self.p_correct = 0;
+            self.p_incorrect = 0;
+            self.counter = 0;
         end
         
         % resetting the model
         function reset(self)
-            self.W_inh      = self.Cprob(meshgrid(self.Theta)...
+            self.W_inh = self.Cprob(meshgrid(self.Theta)...
                 -meshgrid(self.Theta)',...
                 self.a_i,self.c_i);
-            self.Eta        = self.eta*ones(self.N);
-            self.OD         = self.OD_0;
+            self.Eta = self.eta*ones(self.N);
+            self.OD = self.OD_0;
         end
         
         % setting orientation difference
         function set_OD(self,varargin)
-            p               = inputParser;
+            p = inputParser;
             addOptional(p,'OD',[]);
             p.parse(varargin{:});
-            od              = p.Results.OD;
+            od = p.Results.OD;
             if ~isempty(od)
-                self.OD         = od;
+                self.OD = od;
             else
-                self.OD         = self.OD_0;
+                self.OD = self.OD_0;
             end
         end
         
         % setting reference orientation (phi)
         function set_PHI(self,varargin)
-            p               = inputParser;
+            p = inputParser;
             addOptional(p,'PHI',[]);
             p.parse(varargin{:});
-            phi             = p.Results.PHI;
+            phi = p.Results.PHI;
             if ~isempty(phi)
-                self.Phi    = phi;
+                self.Phi = phi;
             else
-                self.Phi    = self.Phi_0;
+                self.Phi = self.Phi_0;
             end
         end
         
-        % setting pp_correct
+        % setting p_correct
         function set_CORRECT(self,p)
            self.p_correct = p; 
         end
         
-        % setting pp_correct
+        % setting p_correct
         function set_INCORRECT(self,p)
            self.p_incorrect = p; 
         end
         
         % getting just noticeable difference
         function JND = get_JND(self)
-            JND             = self.mean_JND;
+            JND = self.mean_JND;
         end
         
         function [We,Wi] = get_weights(self)
-            We              = self.W_exc;
-            Wi              = self.W_inh;
+            We = self.W_exc;
+            Wi = self.W_inh;
         end
         
         function [v,r] = get_response(self,OD)
-            W               = self.W_exc-self.W_inh;
-            V_ff            = self.J_ff*exp(...
+            W = self.W_exc-self.W_inh;
+            V_ff = self.J_ff*exp(...
                 -((self.Adiff(self.Theta,self.Phi + OD)).^2)...
                 /(2*self.sigma_ff^2));
-            [~,v]           = ode45(@(t,v)self.dV(v,V_ff,...
+            [~,v] = ode45(@(t,v)self.dV(v,V_ff,...
                 W,self.alpha,self.tau),...
                 [0 self.t_sim],self.V_0);
-            v               = v(end,:)';
-            r               = self.alpha*max(v,0);
+            v = v(end,:)';
+            r = self.alpha*max(v,0);
         end
         
         % simulation of training session
         function session(self)
-            self.mean_JND   = 0;
-            self.counter    = 0;
+            self.mean_JND = 0;
+            self.counter = 0;
             for t=1:self.trials
                 self.trial();
-                self.mean_JND    = self.mean_JND+...
+                self.mean_JND = self.mean_JND+...
                     (self.OD-self.mean_JND)/t;
             end
         end
@@ -236,51 +236,51 @@ classdef RM < handle
     methods (Access = private)
         % simulation of individual trial
         function trial(self)
-            W               = self.W_exc-self.W_inh;
-            V_ff            = self.J_ff*exp(...
+            W = self.W_exc - self.W_inh;
+            V_ff = self.J_ff * exp(...
                 -((self.Adiff(self.Theta,self.Phi)).^2)...
-                /(2*self.sigma_ff^2));
-            [~,v]           = ode45(@(t,v)self.dV(v,V_ff,...
-                W,self.alpha,self.tau),...
-                [0 self.t_sim],self.V_0);
-            r               = self.alpha*max(v(end,:),0)';
-            M_ref           = r*self.t_sim;
-            Phi_probe       = self.Phi+((rand()>.5)*2-1)*self.OD;
-            V_ff            = self.J_ff*exp(...
+                /(2 * self.sigma_ff^2));
+            [~,v] = ode45(@(t,v)self.dV(v, V_ff, ...
+                W, self.alpha, self.tau),...
+                [0 self.t_sim], self.V_0);
+            r = self.alpha * max(v(end,:), 0)';
+            M_ref = r * self.t_sim;
+            Phi_probe = self.Phi + ((rand()>.5) * 2 - 1) * self.OD;
+            V_ff = self.J_ff * exp(...
                 -((self.Adiff(self.Theta,Phi_probe)).^2)...
-                /(2*self.sigma_ff^2));
-            [~,v]           = ode45(@(t,v)self.dV(v,V_ff,...
-                W,self.alpha,self.tau),...
-                [0 self.t_sim],self.V_0);
-            r               = self.alpha*max(v(end,:),0)';
-            M_probe         = r*self.t_sim;
-            D               = abs(M_ref-M_probe)./...
-                ((self.k*(M_ref+M_probe)).^.5);
-            p               = .5*erfc(-D/(2^.5));
-            p(isnan(p))     = .5;
-            correct         = mean(p>rand(self.N,1))>=self.C;
+                /(2 * self.sigma_ff^2));
+            [~,v] = ode45(@(t,v)self.dV(v, V_ff, ...
+                W, self.alpha, self.tau),...
+                [0 self.t_sim], self.V_0);
+            r = self.alpha * max(v(end,:),0)';
+            M_probe = r * self.t_sim;
+            D = abs(M_ref - M_probe)./...
+                ((self.k * (M_ref+M_probe)).^.5);
+            p = .5 * erfc(-D/(2^.5));
+            p(isnan(p)) = .5;
+            correct = mean(p>rand(self.N,1))>=self.C;
             
             if ~correct
                 if rand() < (1 - self.p_correct)
-                    dW_inh          = self.eta*((1-self.W_inh/...
+                    dW_inh = self.eta*((1-self.W_inh/...
                         (self.J_rec*self.c_i*2^self.a_i)).^self.mu).*...
                         (r*r');
-                    self.W_inh      = self.W_inh+dW_inh;
+                    self.W_inh = self.W_inh+dW_inh;
                 end
-                self.OD         = self.OD*1.2;
-                self.counter    = 1;
+                self.OD = self.OD*1.2;
+                self.counter = 1;
             else
                 if rand() < self.p_incorrect
-                    dW_inh          = self.eta*((1-self.W_inh/...
+                    dW_inh = self.eta*((1-self.W_inh/...
                         (self.J_rec*self.c_i*2^self.a_i)).^self.mu).*...
                         (r*r');
-                    self.W_inh      = self.W_inh+dW_inh;
+                    self.W_inh = self.W_inh+dW_inh;
                 end
                 if self.counter==4
-                    self.OD         = self.OD/1.2;
-                    self.counter    = 1;
+                    self.OD = self.OD/1.2;
+                    self.counter = 1;
                 else
-                    self.counter    = self.counter+1;
+                    self.counter = self.counter+1;
                 end
             end
         end
