352905c22aa242978ff48bc345b7e11b44ecedc0
Szabolcs Meszaros <93373750+skandar98@users.noreply.github.com>
2022-03-23 13:27:15 +0100

    Update main.m

--- FPAnalysis/main.m
+++ FPAnalysis/main.m
@@ -15,22 +15,22 @@ epsilon = 1e-7;
 %
 W1 = W(:,:,4);
 V_ff1 = V_ff(:,4);
-
-
 x = dlarray(V_rec(:,4));
 
 
 [y, grad] = dlfeval(@gradFun,x,V_ff1, W1, alpha, tau);
+grad = extractdata(grad);
+gradient = norm(grad);
 
 %[x1, fval, exitflag, output] = fmin_adam(dlfeval(@gradFun,x,V_ff1, W1, alpha, tau), x);
 
 function y = qfun(V_rec, V_ff1, W1, alpha, tau)
     dV = (-V_rec+V_ff1+W1.*alpha.*max(V_rec,0))./tau;
-    y =sum(dV.^2, 'all')./512;
+    y =sum(dV.^2, 1)./512;
 end
 
 function [y,grad] = gradFun(x, V_ff1, W1, alpha, tau)
     y = qfun(x, V_ff1, W1, alpha, tau);
-    grad = dlgradient(y,x);
+    grad = dlgradient(sum(y,'all'),x);
 end
 
